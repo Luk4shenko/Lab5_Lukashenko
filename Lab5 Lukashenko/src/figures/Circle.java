c56247ce54a379b257ebacbaa1bb6d696db78bea
Luk4shenko <zushdesign@yandex.ru>
2021-11-29 10:23:26 -0800

    Lab5

---
+++ Lab5 Lukashenko/src/figures/Circle.java
@@ -0,0 +1,23 @@
+package figures;
+
+import java.awt.*;
+import java.util.Random;
+import lab5.Shape;
+public class Circle extends Shape {
+
+
+    @Override
+    protected int FirstParameters() {
+        return 10 + (int) (Math.random() * 50);
+    }
+
+    @Override
+    protected int SecondParameters() {
+        return 10 + (int) (Math.random() * 50); }
+
+    @Override
+    protected Color setColor() {
+        return getColor();
+    }
+
+}
---
+++ Lab5 Lukashenko/src/lab5/ShapeViewerApp.java
@@ -0,0 +1,9 @@
+package lab5;
+
+public class ShapeViewerApp {
+    public static void main(String[] args) {
+        ShapeViewer app = new ShapeViewer();
+        app.setVisible(true);
+
+    }
+}
